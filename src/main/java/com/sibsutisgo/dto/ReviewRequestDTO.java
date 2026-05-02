e74bb035ac2fae46818b22acc48f408f205f3e1b
imzhimolost <shinkarenkokseniainfo@gmail.com>
2026-05-02 16:56:45 +0700

    Added DTO for review requests

---
+++ src/main/java/com/sibsutisgo/dto/ReviewRequestDTO.java
@@ -0,0 +1,19 @@
+package com.sibsutisgo.dto;
+
+public class ReviewRequestDTO {
+    private Long id;
+    private Integer rating;
+    private String description;
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public Integer getRating() { return rating; }
+
+    public void setRating(Integer rating) { this.rating = rating; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+}
